086d74e32e8b99b0b61ae2cbebbc6dc4b7ffd993
kazuto1210 <kazuto.iguchi@gmail.com>
2025-04-20 02:54:43 +0900

    update: use PostgreSQL on Render

--- app/settings.py
+++ app/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'multisite_db'),
+        'USER': os.getenv('DB_USER', 'multisite_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', '5xdhKDHuCzKLZ4v4SIGsRvgH4Mm2SrAm'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 # Password validation
