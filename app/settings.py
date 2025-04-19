2b0e45375cd6c0ab344851f83cd697acfc42a7e7
kazuto1210 <kazuto.iguchi@gmail.com>
2025-04-20 03:14:22 +0900

    fix:host

--- app/settings.py
+++ app/settings.py
@@ -81,11 +81,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'multisite_db'),
         'USER': os.getenv('DB_USER', 'multisite_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', '5xdhKDHuCzKLZ4v4SIGsRvgH4Mm2SrAm'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'your_db_password'),
         'HOST': os.getenv('DB_HOST', 'dpg-d01u5iidbo4c7394sddg-a'),
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 
