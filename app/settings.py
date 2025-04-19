2b450e382bae4fac834188f44f8f866ee4f4370c
kazuto1210 <kazuto.iguchi@gmail.com>
2025-04-20 03:20:18 +0900

    fix:db

--- app/settings.py
+++ app/settings.py
@@ -82,10 +82,11 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', 'multisite_db'),
         'USER': os.getenv('DB_USER', 'multisite_user'),
         'PASSWORD': os.getenv('DB_PASSWORD', '5xdhKDHuCzKLZ4v4SIGsRvgH4Mm2SrAm'),
-        'HOST': os.getenv('DB_HOST', 'dpg-d01u5iidbo4c7394sddg-a'),
+        'HOST': os.getenv('DB_HOST', 'dpg-d01u5iidbo4c7394sddg-a.singapore-postgres.render.com'), 
         'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
+
 
 
 
